59eb33f0f1e0815d13616105f12159de5773b40b
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2024-10-30 11:00:08 +0200

    save

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {StyleSheet, TouchableOpacity, Animated, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import LinearGradient from 'react-native-linear-gradient';
 import Entypo from 'react-native-vector-icons/Entypo';
@@ -12,6 +12,7 @@ import {Dimensions} from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const Tab = createBottomTabNavigator();
+import Layaut from './components/Layaut';
 
 import HomeRoute from './routes/HomeRoute';
 import PhotoAlbymRoute from './routes/PhotoAlbymRoute';
@@ -20,212 +21,270 @@ import ProfileRoute from './routes/ProfileRoute';
 import ScheduleRoute from './routes/ScheduleRoute';
 
 const App = () => {
+  ///////// Louder
+  const [louderIsEnded, setLouderIsEnded] = useState(false);
+  const appearingAnim = useRef(new Animated.Value(0)).current;
+  const appearingSecondAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(appearingAnim, {
+      toValue: 1,
+      duration: 3500,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      Animated.timing(appearingSecondAnim, {
+        toValue: 1,
+        duration: 3500,
+        useNativeDriver: true,
+      }).start();
+      //setLouderIsEnded(true);
+    }, 3500);
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLouderIsEnded(true);
+    }, 5000);
+  }, []);
+
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="HomeRoute"
-        screenOptions={{
-          headerShown: false,
-          tabBarShowLabel: false,
-          tabBarActiveBackgroundColor: '#fff',
-          tabBarInactiveBackgroundColor: 'rgba(255, 255, 255, 0.0)', // трошки прозорий білий
-          tabBarStyle: {
-            position: 'absolute',
-            backgroundColor: 'rgba(255, 255, 255, 0.0)', // прозорий фон для таббара
-            borderRadius: 15, // закруглені краї
-            margin: 10, // відступи з країв екрану
-            marginBottom: 20, // відступ від низу
-            height: 60, // висота таббара
-            shadowColor: '#000', // колір тіні
-            shadowOffset: {width: 0, height: 5}, // зміщення тіні
-            shadowOpacity: 0.3, // прозорість тіні
-            shadowRadius: 10, // розмиття тіні
-            elevation: 5, // висота тіні для Android
-          },
-        }}>
-        <Tab.Screen
-          name="ProfileRoute"
-          component={ProfileRoute}
-          options={{
-            tabBarButton: props => (
-              <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
-                <LinearGradient
-                  colors={['red', 'blue']} // градієнт від синього до червоного
-                  start={{x: 0, y: 0}}
-                  end={{x: 1, y: 1}}
-                  style={{
-                    width: windowWidth / 6,
-                    height: 60,
-                    borderRadius: 30,
-                    borderWidth: 3,
-                    borderColor: '#fbc02e',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  {props.children}
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-            tabBarIcon: ({focused}) => {
-              return (
-                <Entypo
-                  name="man"
-                  style={{
-                    fontSize: 30,
-                    color: focused ? '#fbc02e' : '#6469a9',
-                  }}
-                />
-              );
+      {!louderIsEnded ? (
+        <Layaut>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'rgba(0, 0, 255, 0.1)',
+            }}>
+            <Animated.View
+              style={{
+                opacity: appearingAnim,
+                transform: [{scale: appearingAnim}],
+              }}>
+              <Text
+                style={{
+                  color: '#fbc02e',
+                  fontSize: 60,
+                  textAlign: 'center',
+                  fontFamily: 'PlaywriteGBS-Italic',
+                }}>
+                Welcome to "Brisbane Quest: Know Your City" App!!!
+              </Text>
+            </Animated.View>
+          </View>
+        </Layaut>
+      ) : (
+        <Tab.Navigator
+          initialRouteName="HomeRoute"
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveBackgroundColor: '#fff',
+            tabBarInactiveBackgroundColor: 'rgba(255, 255, 255, 0.0)', // трошки прозорий білий
+            tabBarStyle: {
+              position: 'absolute',
+              backgroundColor: 'rgba(255, 255, 255, 0.0)', // прозорий фон для таббара
+              borderRadius: 15, // закруглені краї
+              margin: 10, // відступи з країв екрану
+              marginBottom: 20, // відступ від низу
+              height: 60, // висота таббара
+              shadowColor: '#000', // колір тіні
+              shadowOffset: {width: 0, height: 5}, // зміщення тіні
+              shadowOpacity: 0.3, // прозорість тіні
+              shadowRadius: 10, // розмиття тіні
+              elevation: 5, // висота тіні для Android
             },
-          }}
-        />
+          }}>
+          <Tab.Screen
+            name="ProfileRoute"
+            component={ProfileRoute}
+            options={{
+              tabBarButton: props => (
+                <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
+                  <LinearGradient
+                    colors={['red', 'blue']} // градієнт від синього до червоного
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 1}}
+                    style={{
+                      width: windowWidth / 6,
+                      height: 60,
+                      borderRadius: 30,
+                      borderWidth: 3,
+                      borderColor: '#fbc02e',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    {props.children}
+                  </LinearGradient>
+                </TouchableOpacity>
+              ),
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Entypo
+                    name="man"
+                    style={{
+                      fontSize: 30,
+                      color: focused ? '#fbc02e' : '#6469a9',
+                    }}
+                  />
+                );
+              },
+            }}
+          />
 
-        <Tab.Screen
-          name="PhotoAlbymRoute"
-          component={PhotoAlbymRoute}
-          options={{
-            tabBarButton: props => (
-              <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
-                <LinearGradient
-                  colors={['red', 'blue']} // градієнт від синього до червоного
-                  start={{x: 0, y: 0}}
-                  end={{x: 1, y: 1}}
-                  style={{
-                    width: windowWidth / 6,
-                    height: 60,
-                    borderRadius: 30,
-                    borderWidth: 3,
-                    borderColor: '#fbc02e',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  {props.children}
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-            tabBarIcon: ({focused}) => {
-              return (
-                <FontAwesome5
-                  name="map-marked-alt"
-                  style={{
-                    fontSize: 30,
-                    color: focused ? '#fbc02e' : '#6469a9',
-                  }}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="HomeRoute"
-          component={HomeRoute}
-          options={{
-            tabBarButton: props => (
-              <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
-                <LinearGradient
-                  colors={['red', 'blue']} // градієнт від синього до червоного
-                  start={{x: 0, y: 0}}
-                  end={{x: 1, y: 1}}
-                  style={{
-                    width: windowWidth / 6,
-                    height: 60,
-                    borderRadius: 30,
-                    borderWidth: 3,
-                    borderColor: '#fbc02e',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  {props.children}
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-            tabBarIcon: ({focused}) => {
-              return (
-                <Entypo
-                  name="home"
-                  style={{
-                    fontSize: 50,
-                    color: focused ? '#fbc02e' : '#6469a9',
-                  }}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name="ScheduleRoute"
-          component={ScheduleRoute}
-          options={{
-            tabBarButton: props => (
-              <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
-                <LinearGradient
-                  colors={['red', 'blue']} // градієнт від синього до червоного
-                  start={{x: 0, y: 0}}
-                  end={{x: 1, y: 1}}
-                  style={{
-                    width: windowWidth / 6,
-                    height: 60,
-                    borderRadius: 30,
-                    borderWidth: 3,
-                    borderColor: '#fbc02e',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  {props.children}
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-            tabBarIcon: ({focused}) => {
-              return (
-                <MaterialIcons
-                  name="schedule"
-                  style={{
-                    fontSize: 30,
-                    color: focused ? '#fbc02e' : '#6469a9',
-                  }}
-                />
-              );
-            },
-          }}
-        />
+          <Tab.Screen
+            name="PhotoAlbymRoute"
+            component={PhotoAlbymRoute}
+            options={{
+              tabBarButton: props => (
+                <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
+                  <LinearGradient
+                    colors={['red', 'blue']} // градієнт від синього до червоного
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 1}}
+                    style={{
+                      width: windowWidth / 6,
+                      height: 60,
+                      borderRadius: 30,
+                      borderWidth: 3,
+                      borderColor: '#fbc02e',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    {props.children}
+                  </LinearGradient>
+                </TouchableOpacity>
+              ),
+              tabBarIcon: ({focused}) => {
+                return (
+                  <FontAwesome5
+                    name="map-marked-alt"
+                    style={{
+                      fontSize: 30,
+                      color: focused ? '#fbc02e' : '#6469a9',
+                    }}
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="HomeRoute"
+            component={HomeRoute}
+            options={{
+              tabBarButton: props => (
+                <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
+                  <LinearGradient
+                    colors={['red', 'blue']} // градієнт від синього до червоного
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 1}}
+                    style={{
+                      width: windowWidth / 6,
+                      height: 60,
+                      borderRadius: 30,
+                      borderWidth: 3,
+                      borderColor: '#fbc02e',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    {props.children}
+                  </LinearGradient>
+                </TouchableOpacity>
+              ),
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Entypo
+                    name="home"
+                    style={{
+                      fontSize: 50,
+                      color: focused ? '#fbc02e' : '#6469a9',
+                    }}
+                  />
+                );
+              },
+            }}
+          />
+          <Tab.Screen
+            name="ScheduleRoute"
+            component={ScheduleRoute}
+            options={{
+              tabBarButton: props => (
+                <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
+                  <LinearGradient
+                    colors={['red', 'blue']} // градієнт від синього до червоного
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 1}}
+                    style={{
+                      width: windowWidth / 6,
+                      height: 60,
+                      borderRadius: 30,
+                      borderWidth: 3,
+                      borderColor: '#fbc02e',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    {props.children}
+                  </LinearGradient>
+                </TouchableOpacity>
+              ),
+              tabBarIcon: ({focused}) => {
+                return (
+                  <MaterialIcons
+                    name="schedule"
+                    style={{
+                      fontSize: 30,
+                      color: focused ? '#fbc02e' : '#6469a9',
+                    }}
+                  />
+                );
+              },
+            }}
+          />
 
-        <Tab.Screen
-          name="SetingsRoute"
-          component={SetingsRoute}
-          options={{
-            tabBarButton: props => (
-              <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
-                <LinearGradient
-                  colors={['red', 'blue']} // градієнт від синього до червоного
-                  start={{x: 0, y: 0}}
-                  end={{x: 1, y: 1}}
-                  style={{
-                    width: windowWidth / 6,
-                    height: 60,
-                    borderRadius: 30,
-                    borderWidth: 3,
-                    borderColor: '#fbc02e',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                  }}>
-                  {props.children}
-                </LinearGradient>
-              </TouchableOpacity>
-            ),
-            tabBarIcon: ({focused}) => {
-              return (
-                <Ionicons
-                  name="settings"
-                  style={{
-                    fontSize: 30,
-                    color: focused ? '#fbc02e' : '#6469a9',
-                  }}
-                />
-              );
-            },
-          }}
-        />
-      </Tab.Navigator>
+          <Tab.Screen
+            name="SetingsRoute"
+            component={SetingsRoute}
+            options={{
+              tabBarButton: props => (
+                <TouchableOpacity {...props} style={{flex: 1, marginRight: 10}}>
+                  <LinearGradient
+                    colors={['red', 'blue']} // градієнт від синього до червоного
+                    start={{x: 0, y: 0}}
+                    end={{x: 1, y: 1}}
+                    style={{
+                      width: windowWidth / 6,
+                      height: 60,
+                      borderRadius: 30,
+                      borderWidth: 3,
+                      borderColor: '#fbc02e',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    {props.children}
+                  </LinearGradient>
+                </TouchableOpacity>
+              ),
+              tabBarIcon: ({focused}) => {
+                return (
+                  <Ionicons
+                    name="settings"
+                    style={{
+                      fontSize: 30,
+                      color: focused ? '#fbc02e' : '#6469a9',
+                    }}
+                  />
+                );
+              },
+            }}
+          />
+        </Tab.Navigator>
+      )}
     </NavigationContainer>
   );
 };
